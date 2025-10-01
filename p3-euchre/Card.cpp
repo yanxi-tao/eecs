23493df51ebda5a2fd15b6584841dd0b8dc1a8a9
yanxitao <yanxi.tt@gmail.com>
2025-10-01 13:58:56 -0400

    p3 starter

---
+++ p3-euchre/Card.cpp
@@ -0,0 +1,107 @@
+#include <cassert>
+#include <iostream>
+#include <array>
+#include "Card.hpp"
+
+using namespace std;
+
+/////////////// Rank operator implementations - DO NOT CHANGE ///////////////
+
+constexpr const char *const RANK_NAMES[] = {
+  "Two",   // TWO
+  "Three", // THREE
+  "Four",  // FOUR
+  "Five",  // FIVE
+  "Six",   // SIX
+  "Seven", // SEVEN
+  "Eight", // EIGHT
+  "Nine",  // NINE
+  "Ten",   // TEN
+  "Jack",  // JACK
+  "Queen", // QUEEN
+  "King",  // KING
+  "Ace"    // ACE
+};
+
+//REQUIRES str represents a valid rank ("Two", "Three", ..., "Ace")
+//EFFECTS returns the Rank corresponding to str, for example "Two" -> TWO
+Rank string_to_rank(const std::string &str) {
+  for(int r = TWO; r <= ACE; ++r) {
+    if (str == RANK_NAMES[r]) {
+      return static_cast<Rank>(r);
+    }
+  }
+  assert(false); // Input string didn't match any rank
+  return {};
+}
+
+//EFFECTS Prints Rank to stream, for example "Two"
+std::ostream & operator<<(std::ostream &os, Rank rank) {
+  os << RANK_NAMES[rank];
+  return os;
+}
+
+//REQUIRES If any input is read, it must be a valid rank
+//EFFECTS Reads a Rank from a stream, for example "Two" -> TWO
+std::istream & operator>>(std::istream &is, Rank &rank) {
+  string str;
+  if(is >> str) {
+    rank = string_to_rank(str);
+  }
+  return is;
+}
+
+
+
+/////////////// Suit operator implementations - DO NOT CHANGE ///////////////
+
+constexpr const char *const SUIT_NAMES[] = {
+  "Spades",   // SPADES
+  "Hearts",   // HEARTS
+  "Clubs",    // CLUBS
+  "Diamonds", // DIAMONDS
+};
+
+//REQUIRES str represents a valid suit ("Spades", "Hearts", "Clubs", or "Diamonds")
+//EFFECTS returns the Suit corresponding to str, for example "Clubs" -> CLUBS
+Suit string_to_suit(const std::string &str) {
+  for(int s = SPADES; s <= DIAMONDS; ++s) {
+    if (str == SUIT_NAMES[s]) {
+      return static_cast<Suit>(s);
+    }
+  }
+  assert(false); // Input string didn't match any suit
+  return {};
+}
+
+//EFFECTS Prints Suit to stream, for example "Spades"
+std::ostream & operator<<(std::ostream &os, Suit suit) {
+  os << SUIT_NAMES[suit];
+  return os;
+}
+
+//REQUIRES If any input is read, it must be a valid suit
+//EFFECTS Reads a Suit from a stream, for example "Spades" -> SPADES
+std::istream & operator>>(std::istream &is, Suit &suit) {
+  string str;
+  if (is >> str) {
+    suit = string_to_suit(str);
+  }
+  return is;
+}
+
+
+/////////////// Write your implementation for Card below ///////////////
+
+
+// NOTE: We HIGHLY recommend you check out the operator overloading
+// tutorial in the project spec before implementing
+// the following operator overload functions:
+//   operator<<
+//   operator>>
+//   operator<
+//   operator<=
+//   operator>
+//   operator>=
+//   operator==
+//   operator!=
